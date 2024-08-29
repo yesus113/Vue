9fa6cbcb1bb013b89b6368c09ce7bb7cf6354491
Jesus Adrian <jesusadrianjeronimo113@gmail.com>
2024-08-29 13:56:43 -0600

    Creacion del componente Contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,86 @@
+<template>
+  <h1>Hola mundo</h1>
+  <p>{{ contador }}</p>
+  <button @click="incrementar">INCREMENTAR</button>
+  <button @click="decrementar">DECREMENTAR</button>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const contador = ref(0);
+const incrementar = () => {
+  contador.value++;
+};
+const decrementar = () => {
+  contador.value--;
+}
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
