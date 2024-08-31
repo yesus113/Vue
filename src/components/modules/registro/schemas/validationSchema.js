0d84c27b6b206c29d458e405e7f173baf0a7df03
Jesus Adrian <jesusadrianjeronimo113@gmail.com>
2024-08-31 15:56:41 -0600

    Modificacion pequeña para la validacion del form

--- src/components/modules/registro/schemas/validationSchema.js
+++ src/components/modules/registro/schemas/validationSchema.js
@@ -1,5 +1,6 @@
 import * as yup from 'yup';
- export const schema = yup.object({
-    nombre: yup.string().required,
-    email: yup.string().email().required
-})
+
+export const schema = yup.object({
+  nombre: yup.string().required('El nombre es obligatorio'),
+  email: yup.string().email('Debe ser un correo válido').required('El correo es obligatorio')
+});
